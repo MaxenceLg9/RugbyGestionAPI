58257f631f2551ac5a12b860336099048f87c236
maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-03-06 13:50:25 +0100

    fichier de connexion à la db

---
+++ www/db/db.php
@@ -0,0 +1,9 @@
+<?php
+
+function getPDO():PDO
+{
+    $dsn = 'mysql:host=localhost;dbname=rugbygestion;charset=utf8';
+    $username = 'root';
+    $password = 'pq04WX11me2005!';
+    return new PDO($dsn, $username, $password);
+}
