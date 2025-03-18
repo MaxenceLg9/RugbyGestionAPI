3800e3d63beb036d1f80627b4dbca374c0ca486a
maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-03-18 14:44:20 +0100

    remplissage de la fdm fait avec POST (insert,update,delete) Récupération avec GET & les get parameters PATCH : saisie des notes PUT : archivage / validation

--- www/public/fdm.php
+++ www/public/fdm.php
@@ -1,23 +1,107 @@
 <?php
 
-require_once "../db/DAOJouerUnMatch.php";
+require_once "../libs/modele/FDM.php";
+
+use function FDM\isArchiveFDM;
+use function FDM\readByNumeroAndMatch,FDM\read,FDM\readByMatch,FDM\readByJoueur,FDM\fillFDM,FDM\archiver,FDM\deleteMatch,\FDM\setNotes;
 
 header("Content-Type: application/json");
 
+//check du jeton avec une requête POST sur l'api d'authentification
+/*
+if(!apiVerifyToken()){
+
+}
+*/
+
 $jsonBody = json_decode(file_get_contents('php://input'), true);
 
-if($_SERVER['REQUEST_METHOD'] == 'GET') {
-    if(isset($_GET['idMatch'])) {
-        $message = array("status" => 200, "response" => "Feuille de match récupérée avec succès", "data" => DAOJouerUnMatch::readAllByMatch($_GET["idMatch"]));
-    } else {
-        $message = array("status" => 200, "response" => "Liste des feuilles de matchs récupérées avec succès", "data" => DAOJouerUnMatch::readAll());
+/**
+ * @param mixed $jsonBody
+ * @return bool
+ */
+function checkPOSTBody(mixed $jsonBody): bool
+{
+    if(!isset($jsonBody["idMatch"]) || !isset($jsonBody["feuilles"]))
+        return false;
+    foreach ($jsonBody["feuilles"] as $key => $value) {
+        if (!isset($key) || !is_numeric($key) || $key < 1 || $key > 23)
+            return false;
+        if(!isset($value["idJoueur"]) || !isset($value["estTitulaire"]))
+            return false;
     }
-} else if ($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $message = array("status" => 405, "response" => "Méthode non implémentée");
-} else {
+    return true;
+}
+function checkPATCHBody(mixed $jsonBody): bool {
+    if(!isset($jsonBody["idMatch"]) || !isset($jsonBody["feuilles"]))
+        return false;
+    foreach ($jsonBody["feuilles"] as $key => $value) {
+        if (!isset($key) || !is_numeric($key) || $key < 1 || $key > 23)
+            return false;
+        if(!isset($value["note"]))
+            return false;
+    }
+    return true;
+}
+
+
+
+
+if($_SERVER['REQUEST_METHOD'] == 'GET') {
+    if(isset($_GET["idMatch"])){
+        if(isset($_GET["numero"]))
+            $message = array("status" => 200, "response" => "Feuille de Match par numéro & match récupérée avec succès", "data" => (readByNumeroAndMatch(array("idMatch" => $_GET["idMatch"],"numero" => $_GET["numero"]))));
+        else
+            $message = array("status" => 200, "response" => "Feuilles de Match du match récupérées avec succès", "data" => (readByMatch($_GET["idMatch"])));
+    } else if(isset($_GET["idJoueur"])){
+        $message = array("status" => 200, "response" => "Feuilles de Match du joueur récupérées avec succès", "data" => (readByJoueur($_GET["idJoueur"])));
+    }
+    else {
+        $message = array("status" => 200, "response" => "Liste des Feuilles de Matchs récupérées avec succès", "data" => (read()));
+    }
+}
+//creer, modifier, supprimer les liaisons entre un joueur et un match
+else if($_SERVER['REQUEST_METHOD'] == 'POST') {
+    if(checkPOSTBody($jsonBody)){
+        if(isArchiveFDM($jsonBody["idMatch"]))
+            $message = array("status" => 422, "response" => "Impossible de mettre à jour la feuille de match : validée", "data" => []);
+        else
+            $message = array("status" => 201, "response" => "Feuille de Match créé avec succès", "data" => fillFDM($jsonBody));
+    } else {
+        $message = array("status" => 400, "response" => "Les paramètres sont invalides");
+    }
+}
+else if($_SERVER["REQUEST_METHOD"] == 'PUT') {
+    if(isset($jsonBody["idMatch"]) && archiver($jsonBody["idMatch"]))
+        $message = array("status" => 200, "response" => "Feuille de match validée avec succès", "data" => readByMatch($jsonBody["idMatch"]));
+    else
+        $message = array("status" => 200, "response" => "Erreur lors de la modification de la Feuille de Match", "data" => []);
+}
+else if($_SERVER["REQUEST_METHOD"] == 'PATCH') {
+    if(checkPATCHBody($jsonBody)){
+        if(!isArchiveFDM($jsonBody["idMatch"]))
+            $message = array("status" => 422, "response" => "Impossible de mettre des notes car la feuille de match n'est pas validée", "data" => []);
+        else
+            $message = array("status" => 200, "response" => "Feuille de Match modifié avec succès", "data" => setNotes($jsonBody["feuilles"],$jsonBody["idMatch"]));
+    } else {
+        $message = array("status" => 400, "response" => "Les paramètres sont invalides");
+    }
+}
+else if($_SERVER['REQUEST_METHOD'] == 'DELETE') {
+    if(isset($jsonBody["idMatch"])){
+        if(deleteMatch($jsonBody["idMatch"]))
+            $message = array("status" => 200, "response" => "Feuille de Match supprimé avec succès", "data" => readByMatch($jsonBody["idMatch"]));
+        else
+            $message = array("status" => 200, "response" => "Erreur lors de la suppression de la Feuille de Match", "data" => []);
+    } else {
+        $message = array("status" => 400, "response" => "Les paramètres sont invalides");
+    }
+}
+else {
     $message = array("status" => 405, "response" => "Méthode non autorisée");
 }
-echo json_encode($message);
 
+http_response_code($message["status"]);
+echo json_encode($message);
 
 
