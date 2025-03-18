a820d1e450e9762605be7c24f12f9f921d0ee0b7
maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-03-18 14:03:53 +0100

    methode pour ajouter,modifier,recuperer & supprimer

--- www/public/matchs.php
+++ www/public/matchs.php
@@ -1,27 +1,56 @@
 <?php
-require_once "../db/DAOMatchDeRugby.php";
+require_once "../libs/modele/Match.php";
 
 header("Content-Type: application/json");
 
+//check du jeton avec une requête POST sur l'api d'authentification
+/*
+if(!apiVerifyToken()){
+
+}
+*/
+
 $jsonBody = json_decode(file_get_contents('php://input'), true);
 
-if($_SERVER['REQUEST_METHOD'] == 'GET') {
-    if(isset($_GET["idMatch"])){
-        $message = array("status" => 200, "response" => "Match récupéré avec succès", "data" => DAOMatchDeRugby::readById($_GET["idMatch"]));
-    } else {
-        $message = array("status" => 200, "response" => "Liste des matchs récupérés avec succès", "data" => DAOMatchDeRugby::read());
-    }
+function validateDate($date, $format = 'Y-m-d H:i:s') : bool {
+    $d = DateTime::createFromFormat($format, $date);
+    // The Y ( 4 digits year ) returns TRUE for any integer with any number of digits so changing the comparison from == to === fixes the issue.
+    return $d && strtolower($d->format($format)) === strtolower($date);
 }
-else if($_SERVER['REQUEST_METHOD'] == 'POST') {
-    $message = array("status" => 405, "response" => "Méthode non implémentée");
-    if(isset($jsonBody[])){
 
-    }
+function checkBody(mixed $jsonBody): bool {
+    return isset($jsonBody["adversaire"]) && isset($jsonBody["dateHeure"]) && isset($jsonBody["lieu"]) && in_array($jsonBody["lieu"], ["DOMICILE", "EXTERIEUR"]) && validateDate($jsonBody["dateHeure"]);
 }
-else if($_SERVER['REQUEST_METHOD'] == 'PATCH') {
 
-}
-else {
+
+if($_SERVER['REQUEST_METHOD'] == 'GET')
+    if(isset($_GET["idMatch"]))
+        $message = array("status" => 200, "response" => "Match récupéré avec succès", "data" => MatchDeRugby::readById($_GET["idMatch"]));
+     else
+        $message = array("status" => 200, "response" => "Liste des Matchs récupérés avec succès", "data" => MatchDeRugby::read());
+
+else if($_SERVER['REQUEST_METHOD'] == 'POST')
+    if(checkBody($jsonBody))
+        $message = array("status" => 201, "response" => "Match créé avec succès", "data" => MatchDeRugby::readById(MatchDeRugby::create($jsonBody)));
+    else
+        $message = array("status" => 400, "response" => "Les paramètres sont invalides");
+
+else if($_SERVER["REQUEST_METHOD"] == 'PUT')
+    if (checkBody($jsonBody) && isset($jsonBody["idMatch"]))
+        if (MatchDeRugby::update($jsonBody))
+            $message = array("status" => 200, "response" => "Match modifié avec succès", "data" => MatchDeRugby::readById($jsonBody["idMatch"]));
+        else
+            $message = array("status" => 200, "response" => "Erreur lors de la modification du Match", "data" => MatchDeRugby::readById($jsonBody["idMatch"]));
+    else
+        $message = array("status" => 400, "response" => "Les paramètres sont invalides");
+
+else if($_SERVER['REQUEST_METHOD'] == 'DELETE')
+    if(isset($jsonBody["idMatch"]))
+        $message = array("status" => 200, "response" => "Match supprimé avec succès", "data" => MatchDeRugby::delete($jsonBody["idMatch"]));
+     else
+        $message = array("status" => 400, "response" => "Les paramètres sont invalides");
+else
     $message = array("status" => 405, "response" => "Méthode non autorisée");
-}
+
+http_response_code($message["status"]);
 echo json_encode($message);
