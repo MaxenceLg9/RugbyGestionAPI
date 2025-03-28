fac18b8d26bb645b7a304c28931f01f8e5341a3a
Lagourgue Maxence <maxence.lagourgue@free.fr>
2025-03-28 21:57:59 +0100

    mise à jour de la connexion

--- www/libs/db/db.php
+++ www/libs/db/db.php
@@ -2,13 +2,14 @@
 
 function getPDO():PDO
 {
-//    $dsn = 'mysql:host=localhost;dbname=rugbygestion;charset=utf8';
-    $dsn = 'mysql:host=db;dbname=rugbygestion';
+    $dsn = 'mysql:host=mysql-gestionequiperugby.alwaysdata.net;dbname=gestionequiperugby_bd;charset=utf8';
+//    $dsn = 'mysql:host=db;dbname=rugbygestion';
 //    $options = [
 //        PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
 //        PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
 //        PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8mb4"
 //    ];
 
-    return new PDO($dsn, 'root', 'pq04WX11me2005!',array(PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8"));
+//    return new PDO($dsn, 'root', 'pq04WX11me2005!');
+    return new PDO($dsn,"387507",'$iutinfo');
 }
