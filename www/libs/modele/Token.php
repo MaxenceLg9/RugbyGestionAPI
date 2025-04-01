5bc766249de213d1d8b6bfe5c3e2c459a89fd872
maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-04-01 21:33:19 +0200

    fonction pour vérifier le token auprès de l'api auth

---
+++ www/libs/modele/Token.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace Token {
+
+    use GuzzleHttp\Client;
+
+    function apiVerifyToken(): bool {
+        $token = get_bearer_token() ?? "";
+        $client = new Client([
+            'base_uri' => 'https://rugbygestionauth.alwaysdata.net/',
+            'timeout'  => 2.0,
+            'verify' => false
+        ]);
+
+        $response = $client->post('/',[
+            'query' => [
+                'token' => $token,
+            ],
+            'headers' => [
+                'Authorization' => $_COOKIE["token"] ?? "",
+                'Accept' => 'application/json',
+                "Content-Type: application/json"
+            ]
+        ]);
+        return json_decode($response->getBody(),true)["valid"];
+    }
+
+    function apiReloadToken(): string {
+        $token = get_bearer_token() ?? "";
+        $client = new Client([
+            'base_uri' => 'https://rugbygestionauth.alwaysdata.net/',
+            'timeout'  => 2.0,
+            'verify' => false
+        ]);
+
+        $response = $client->put('/',[
+            'query' => [
+                'token' => $token,
+            ],
+            'headers' => [
+                'Authorization' => $_COOKIE["token"] ?? "",
+                'Accept' => 'application/json',
+                "Content-Type: application/json"
+            ]
+        ]);
+        return json_decode($response->getBody(),true)["token"];
+    }
+}
