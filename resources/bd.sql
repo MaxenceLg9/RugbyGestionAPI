955851a28dfe3e912d8d1d50bbe0200286582c0d
maxence <maxence.lagourgue@etu.iut-tlse3.fr>
2025-04-03 11:21:33 +0200

    séparation de la bd intiale en bd api & auth

--- resources/bd.sql
+++ resources/bd.sql
@@ -53,18 +53,8 @@ CREATE TABLE IF NOT EXISTS Participer (
     PRIMARY KEY (idMatch, numero),
     FOREIGN KEY (idMatch) REFERENCES MatchDeRugby(idMatch),
     FOREIGN KEY (idJoueur) REFERENCES Joueur(idJoueur)
-    );
-
-CREATE TABLE IF NOT EXISTS Entraineur (
-    idEntraineur INT PRIMARY KEY AUTO_INCREMENT,
-    nom VARCHAR(50) NOT NULL,
-    prenom VARCHAR(50) NOT NULL,
-    email VARCHAR(50) NOT NULL,
-    equipe VARCHAR(20) NOT NULL,
-    motDePasse VARCHAR(60) NOT NULL
-    );
+);
 
 ALTER TABLE Joueur CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE MatchDeRugby CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
 ALTER TABLE Participer CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
-ALTER TABLE Entraineur CONVERT TO CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;
